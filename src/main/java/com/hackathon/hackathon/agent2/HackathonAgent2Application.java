8fb6fb0c6d50ee941765cd77ae896ed20d7fd0a6
AnawaratneMANA <it19086408@my.sliit.lk>
2025-11-13 12:35:44 +0530

    Add the initial files for Agent 2

---
+++ src/main/java/com/hackathon/hackathon/agent2/HackathonAgent2Application.java
@@ -0,0 +1,13 @@
+package com.hackathon.hackathon.agent2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonAgent2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonAgent2Application.class, args);
+	}
+
+}
